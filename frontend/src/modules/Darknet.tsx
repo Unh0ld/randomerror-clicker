e1f27d1e2f1646fa2ab2dedb61ad83bb49101ffd
Henri Schmidt <henri@reschmi.de>
2022-11-30 21:52:09 +0100

    prototype darknet shop

--- frontend/src/modules/Darknet.tsx
+++ frontend/src/modules/Darknet.tsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 
-function Darknet() {
-    return (
-        <div>
-          <div><button >Zero Day</button></div>
-          <div><button >XSS</button></div>
-        </div>
-        );
-  }
+interface Item {
+  name: string;
+  count: number;
+  setter: React.Dispatch<React.SetStateAction<number>>;
+  effect: () => void;
+  price: number;
+}
 
-  export default Darknet;
+function Darknet() {
+  const items = [
+    createItem('Bot', 10, () => {}, useState(0)),
+    createItem('Zero Day', 1000, () => {}, useState(0)),
+    createItem('XSS', 100, () => {}, useState(0)),
+    createItem('Script Kiddy Script', 10000, () => {}, useState(0)),
+  ]
+
+  return (
+    <div>
+      <ul>
+        {
+          items.map((item,index) =>
+            <li key={index}>
+              <button onClick={() => buy(item)}>{item.name}</button>
+              - {item.price} Hacks - {item.count}
+            </li>
+          )
+        }
+      </ul>
+    </div>
+  );
+}
+
+function createItem(name: string, price: number, effect: () => void, stateHook: [number, React.Dispatch<React.SetStateAction<number>>]): Item {
+  const [count, setter] = stateHook;
+  return {
+    name,
+    effect,
+    price,
+    setter,
+    count,
+  };
+}
+
+function buy(item: Item) {
+  
+}
+
+export default Darknet;

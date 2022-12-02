51e36d6c48d99ee2c30e305f09b302a2e89edc19
Rene.Brinkhege <rene.brinkhege@isst.fraunhofer.de>
2022-12-02 21:25:27 +0100

    feat: Prevent negative number of hackers and use useEffect hook for setInterval timer

--- frontend/src/modules/Counter.tsx
+++ frontend/src/modules/Counter.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 /* Counter component with a button that increments a counter.
 additionally it contains a auto-incrementing timer that increments the counter every second by a certain amount called "increment" */
@@ -7,16 +6,34 @@ function Counter() {
     const [hacks, setCounter] = useState(0);
     const [increment, setIncrement] = useState(0);
 
+    // Call the setInterval function when the Counter component is first rendered
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // Increment the hacks counter by the increment value every second
+            setCounter(prevCounter => prevCounter + increment);
+        }, 1000);
+
+        // Clean up the interval when the Counter component is unmounted
+        return () => clearInterval(interval);
+    }, [increment]);
+
     return (
         <div>
             <div>Devices hacked: {hacks}</div>
             <button onClick={() => setCounter(hacks+1)}>Hack 🖧</button>
             <div>Hackers: {increment}</div>
             <button onClick={() => setIncrement(prevIncrement => prevIncrement+1)}>Hire 🧑‍💻</button>
-            <button onClick={() => setIncrement(prevIncrement => prevIncrement-1)}>Fire 🧑‍🔧</button>
+            <button
+                onClick={() => {
+                    // Check if the increment value is greater than 0 before decrementing it
+                    if (increment > 0) {
+                        setIncrement(prevIncrement => prevIncrement-1);
+                    }
+                }}
+            >
+                Fire 🧑‍🔧
+            </button>
             <button onClick={() => setIncrement(0)}>Reset Hackers</button>
-
-            <button onClick={() => setInterval(() => setCounter(prevCounter => prevCounter + increment), 1000)}>Start Hackers</button>     
         </div>
         );
   }

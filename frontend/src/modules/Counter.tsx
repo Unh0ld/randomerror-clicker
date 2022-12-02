4886a9a95b2986ae3930286f0da7c24ef7a59a05
Rene.Brinkhege <rene.brinkhege@isst.fraunhofer.de>
2022-12-02 22:08:11 +0100

    fix: use ref for increment in Counter component
    The timer in the Counter component was not updating with the latest increment value because it was using the initial value of the increment state variable. This commit fixes the problem by using a ref to store the current value of the increment state variable and updating the ref whenever the increment state variable is updated.

--- frontend/src/modules/Counter.tsx
+++ frontend/src/modules/Counter.tsx
@@ -1,33 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-/* Counter component with a button that increments a counter.
-additionally it contains a auto-incrementing timer that increments the counter every second by a certain amount called "increment" */
 function Counter() {
     const [hacks, setCounter] = useState(0);
     const [increment, setIncrement] = useState(0);
 
+    // Create a ref to store the current value of the increment state variable
+    const incrementRef = useRef(increment);
+
     // Call the setInterval function when the Counter component is first rendered
     useEffect(() => {
         const interval = setInterval(() => {
-            // Increment the hacks counter by the increment value every second
-            setCounter(prevCounter => prevCounter + increment);
+            // Use the value of the increment ref to increment the hacks counter
+            setCounter(prevCounter => prevCounter + incrementRef.current);
         }, 1000);
 
         // Clean up the interval when the Counter component is unmounted
         return () => clearInterval(interval);
+    }, []);
+
+    // Update the value of the increment ref when the increment state variable is updated
+    useEffect(() => {
+        incrementRef.current = increment;
     }, [increment]);
 
     return (
         <div>
             <div>Devices hacked: {hacks}</div>
-            <button onClick={() => setCounter(hacks+1)}>Hack 🖧</button>
+            <button onClick={() => setCounter(hacks + 1)}>Hack 🖧</button>
             <div>Hackers: {increment}</div>
-            <button onClick={() => setIncrement(prevIncrement => prevIncrement+1)}>Hire 🧑‍💻</button>
+            <button onClick={() => setIncrement(prevIncrement => prevIncrement + 1)}>Hire 🧑‍💻</button>
             <button
                 onClick={() => {
                     // Check if the increment value is greater than 0 before decrementing it
                     if (increment > 0) {
-                        setIncrement(prevIncrement => prevIncrement-1);
+                        setIncrement(prevIncrement => prevIncrement - 1);
                     }
                 }}
             >
@@ -35,7 +41,7 @@ function Counter() {
             </button>
             <button onClick={() => setIncrement(0)}>Reset Hackers</button>
         </div>
-        );
-  }
+    );
+}
 
-  export default Counter;
+export default Counter;

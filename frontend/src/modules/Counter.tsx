b2e23451e40e06973ef33c7267c80966f39ad579
Henri Schmidt <henri@reschmi.de>
2022-12-09 22:06:59 +0100

    fix linting

--- frontend/src/modules/Counter.tsx
+++ frontend/src/modules/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './Counter.css'; // Import the stylesheet
 
 function Counter() {
